7202862a510db7967bb4054e73fd604c8859931a
Chris White <chris.matthew.white@gmail.com>
2014-12-15 21:42:55 -0500

    beginning project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wintercarnival::Application.config.secret_key_base = '68bcd890f05f5dd048344512dfcf251ccb6ba03d54fde2fcc67b397a6249f826996e7acf5cbd780653f86c0dd24e1c3b52c916b60f7ba205e5a68b5019106f25'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wintercarnival::Application.config.session_store :cookie_store, key: '_wintercarnival_session'
